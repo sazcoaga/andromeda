4ced8d75f7dd8b09d7ee0ee6a1d1cf22b21a0b61
sazcoaga <sazcoaga@est.frba.utn.edu.ar>
2020-10-21 16:32:50 -0300

    cambios

--- javascript.js
+++ javascript.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded",
     function sayHello (event) {
       console.log(event);
 
-      this.textContent = "Said it!";
+      this.textContent = "Probar";
       var name =  document.getElementById("name").value;
-        if(name === "29032510"){ var message = "<p>Bien! Entraste! Cómo no se te ocurrió antes?</p>"}
-          else {var message = "<p> Mmmm, esa no es. Pero te dejo probar otra vez</p>"}
+        if(name === "29032510"){ var message = "<p1> ncteoaonr al rabo de rtea </p1>"}
+          else {var message = "<p2> Mmmm, esa no es. Pero te dejo probar otra vez</p2>"}
        
 
       document
